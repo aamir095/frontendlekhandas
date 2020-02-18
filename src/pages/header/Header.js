5179fa22226217d05751c9dd7e97f92143572d03
achsantosh <achsantosh3150@gmail.com>
2020-02-18 12:58:05 +0545

    navbar created

--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import Title from './Title';
+import Flexbox from 'flexbox-react';
+import Img from 'react-image';
+import './Header.scss';
 
 export default class Header extends React.Component{
-    
- handleChange(e){
-     const title= e.target.value;
-     this.props.changeTitle(title);
- }
+
+
 
  
     
     render(){
-        
         return(
             <div>
-                <Title title={this.props.title}/>
-                <input onChange={this.handleChange.bind(this)}/> 
+                <div className="navbar" >
+                <div className="center">
+                    <div className="stylenav">
+                    <div className="logo">
+                        <a href="#">
+                        <img src="../../assets/lekhandas.png"/>
+                        </a>
+                    </div>
+                    <div className="navmenu">
+                        <div className="navitem">
+                            <a href="#">Home</a>
+                            <a href="#">Services</a>
+                            <a href="#">About Us</a>
+                            <a href="#">Gallery</a>
+                        </div>
+                        <div className="loginsignup">
+                            <span>
+                            <a href="#">Sign Up</a>
+                            </span>
+                            <span>
+                            <a href="#">Sign In</a>
+                            </span>
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+
+
             </div>
         );
     }
