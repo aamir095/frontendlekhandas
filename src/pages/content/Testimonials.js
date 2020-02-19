cda6c53b0a4d5caed58a83f7b149c2c02c8c66a0
bioAttendance <amirpaudel@machineertech.com>
2020-02-19 14:55:19 +0545

    testimonial added

---
+++ src/pages/content/Testimonials.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+
+export default class Testimonials extends React.Component{
+    render(){
+        return(
+
+            <div className="testimonials">
+                This is Testimonial Page.
+            </div>
+        );
+    }
+
+ }
